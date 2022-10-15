a939b0b3260281e4f8f78fa4d22668250b36cbf8
Neethu <neethusebastiankoroth@gmail.com>
2022-10-15 16:49:30 +0530

    Init commit

---
+++ src/test/java/BaseClass.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("http://orangehrm.qedgetech.com/symfony/web/index.php/auth/login");
+        driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("Admin123");
+        driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+
+    }
+    }
